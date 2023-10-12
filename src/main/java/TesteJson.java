4971a72e11eccef6ae246d0f029463f02c8ee4a4
FernandaFonseca07 <fernanda_fonseca0107@hotmail.com>
2023-10-12 17:57:37 -0300

    Leitura e escrita Json

--- src/main/java/TesteJson.java
+++ src/main/java/TesteJson.java
@@ -1,8 +1,6 @@
 import com.google.gson.Gson;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +9,30 @@ public class TesteJson {
     static Gson gson = new Gson(); //Biblioteca do goole para fazer manipulção do Json
     static Reader reader;
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        leituraJson();
+        escritaJson();
+    }
+
+    public static void leituraJson () throws FileNotFoundException {
         reader = new FileReader("C:\\Users\\Fernanda\\Desktop\\QA ACADEMY\\selenium_cucumber\\persistencia_json\\src\\main\\resources\\massaTeste.json");
         dadosTesteMap = gson.fromJson(reader, Map.class);
 
         System.out.println(dadosTesteMap.get("url"));
         System.out.println(dadosTesteMap.get("produto"));
         System.out.println(dadosTesteMap.get("valor"));
-
     }
+
+    public static void escritaJson () throws IOException {
+        dadosTesteMap.put("email", "teste@teste.com");
+        dadosTesteMap.put("senha", "teste@teste");
+        FileWriter writer = new FileWriter("C:\\Users\\Fernanda\\Desktop\\QA ACADEMY\\selenium_cucumber\\persistencia_json\\src\\main\\resources\\copia_massaTeste.json");
+        gson.toJson(dadosTesteMap, writer);
+        writer.flush();
+        writer.close();
+        System.out.println(dadosTesteMap.get("email"));
+        System.out.println(dadosTesteMap.get("senha"));
+    }
+
+
 }
